1adb7fc3db445a58446e69fb93d8cf792ee09975
vianney <vianney.p@outlook.com>
2020-02-10 15:59:17 +0100

    Add default config.

--- config.sh
+++ config.sh
@@ -2,6 +2,8 @@
 
 set -xe
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # Static config
 
 USER_LIST="users.cfg"
@@ -12,14 +14,14 @@ CLUSTER_PRIVATE_KEY="cluster_private.key"
 DATA_DIR_INSECURE="insecure/db"
 LOG_DIR_INSECURE="insecure/log"
 URI_INSECURE="127.0.0.1:2836"
-CONFIG_INSECURE="qdbd_insecure.cfg"
+CONFIG_INSECURE="${SCRIPT_DIR}/default.qdbd.cfg"
 CONSOLE_LOG_INSECURE="qdbd_log_insecure.out.txt"
 CONSOLE_ERR_LOG_INSECURE="qdbd_log_insecure.err.txt"
 
 DATA_DIR_SECURE="secure/db"
 LOG_DIR_SECURE="secure/log"
 URI_SECURE="127.0.0.1:2838"
-CONFIG_SECURE="qdbd_secure.cfg"
+CONFIG_SECURE="${SCRIPT_DIR}/default.qdbd.cfg"
 CONSOLE_LOG_SECURE="qdbd_log_secure.out.txt"
 CONSOLE_ERR_LOG_SECURE="qdbd_log_secure.err.txt"
 
@@ -43,6 +45,12 @@ do
         -i|--node-ids)
             # Node IDS are a comma-separated list
             IFS=', ' read -r -a NODE_IDS <<< "$2"
+            shift # past argument
+            shift # past value
+            ;;
+        --async-flush-deadline)
+            # Async flush deadline in ms
+            ASYNC_FLUSH_DEADLINE=$2
             shift # past argument
             shift # past value
             ;;
