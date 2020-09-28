b19a62d6ae521aae4521ac147e695ab8f7af04b4
Marek Kurdej <mkurdej@users.noreply.github.com>
2020-09-28 10:18:26 +0200

    Refactor binaries script. Do not check for binaries when stopping services. (#1)

--- binaries.sh
+++ binaries.sh
@@ -37,23 +37,34 @@ case "$(uname)" in
     ;;
 esac
 
-FOUND=0
-if [[ ! -f ${QDBD} ]]; then
-    echo "Binary ${QDBD} not found."
-    FOUND=1
-fi
-if [[ ! -f ${QDB_USER_ADD} ]]; then
-    echo "Binary ${QDB_USER_ADD} not found."
-    FOUND=1
-fi
-if [[ ! -f ${QDB_CLUSTER_KEYGEN} ]]; then
-    echo "Binary ${QDB_CLUSTER_KEYGEN} not found."
-    FOUND=1
-fi
-
-if [[ ${FOUND} != 0 ]] ; then
-    echo "Binaries not found. Exiting..."
-    exit 1
-fi
-
 QDBD_FILENAME=${QDBD##*/}
+
+function check_binary {
+    local binary=$1;shift
+
+    if [[ ! -f ${binary} ]]; then
+        echo "Binary ${binary} not found."
+        return 1
+    fi
+    return 0
+}
+
+function check_binaries {
+    FOUND=0
+    set +e
+    if ! check_binary "${QDBD}" ; then
+        FOUND=1
+    fi
+    if ! check_binary "${QDB_CLUSTER_KEYGEN}" ; then
+        FOUND=1
+    fi
+    if ! check_binary "${QDB_USER_ADD}" ; then
+        FOUND=1
+    fi
+    set -e
+
+    if [[ ${FOUND} != 0 ]] ; then
+        echo "Binaries not found. Exiting..."
+        exit 1
+    fi
+}
